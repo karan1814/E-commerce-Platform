58899a6d231f7c875167de2e5cd8b313be5ffdae
karan rathore <karanrathore181403@gmail.com>
2026-03-10 19:25:33 +0530

    Removed node_modules and uploads from tracking

---
+++ backend/models/order.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+    {
+        user:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required: true
+        },
+
+        orderItems: [
+            {
+                name: {type: String, required: true},
+                qty: { type: Number, required: true},
+                image: { type: String, required: true},
+                price: { type: String, required: true},
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Product",
+                    required: true
+
+                }
+            }
+        ],
+
+        shippingAddress: {
+            address: {type: String, required: true},
+            city: {type: String, required: true},
+            postalCode: {type: String, required: true},
+            country: { type:String, required: true}
+        },
+
+        paymentMethod: {
+            type: String,
+            required: true
+        },
+
+        itemsPrice: {
+            type: String,
+            required: true,
+        },
+
+        textPrice: {
+            type: Number,
+            required: true
+        },
+
+        shippingPrice: {
+            type: Number,
+            required: true
+        },
+
+        totalPrice: {
+            type: Number,
+            required: true
+        },
+
+        isPaid: {
+            type: Boolean,
+            default: false
+        },
+
+        paidAt: {
+            type: Date
+        },
+
+        isDelivered: {
+            type: Boolean,
+            default: false
+        },
+
+        deliveredAt: {
+            type: Date
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
